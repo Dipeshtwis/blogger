e3a7e86a6d80ff415b83a3a6a3a94fdf168c4885
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-08-04 17:04:02 +0300

    Add new and create action and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,19 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+
+	def show 
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create 
+		@article = Article.new
+ 		@article.title = params[:article][:title]
+		@article.save
+		redirect_to article_path(@article)
+	end
 end
