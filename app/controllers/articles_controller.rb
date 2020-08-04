96c448fe45f0f3328b1e51553598e8959291f8c2
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-04 20:14:08 +0530

    Add the functionality of editing and deleting of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 	def index
 		@articles = Article.all
 	end
@@ -12,9 +13,26 @@ class ArticlesController < ApplicationController
 	end
 
 	def create 
-		@article = Article.new
- 		@article.title = params[:article][:title]
+		@article = Article.new(article_params)
 		@article.save
 		redirect_to article_path(@article)
 	end
+
+	def destroy
+		@article = Article.find(params[:id]).destroy
+		redirect_to articles_path
+	end
+
+	def edit
+ 	 @article = Article.find(params[:id])
+	end
+
+	def update
+  		@article = Article.find(params[:id])
+  		@article.update(article_params)
+
+  		redirect_to article_path(@article)
+end
+
+
 end
