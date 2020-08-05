cad3af9987300d1d983e5b327d3a0c07835ae1cf
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-08-05 16:48:00 +0300

    Add gem files and upload image functionality for an article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
+  has_attached_file :image
+  validates_attachment_content_type :image, 
+                      :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   def tag_list
     tags.join(", ")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_081307) do
+ActiveRecord::Schema.define(version: 2020_08_05_130203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2020_08_05_081307) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
