cdc588019c1616b65913eefd232bbe73b2ddae9d
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-08-06 11:49:23 +0300

    Change page redirect to blog posts upon login

--- app/controllers/author_sessions_controller.rb
+++ app/controllers/author_sessions_controller.rb
@@ -5,7 +5,7 @@ class AuthorSessionsController < ApplicationController
 
   def create
     if login(params[:email], params[:password])
-      redirect_back_or_to(articles_path, notice: 'Logged in successfully.')
+      redirect_to(articles_path, notice: 'Logged in successfully.')
     else
       flash.now.alert = "Login failed."
       render action: :new
