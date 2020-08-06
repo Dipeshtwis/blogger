75b17d67c305f1a9e87d25dc30fe300305adfd97
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-08-06 14:13:46 +0300

    Add feature for webpack and yarn

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+// ...
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+environment.plugins.prepend("CleanWebpackPlugin", new CleanWebpackPlugin());
+
+// ...
